25026ad4e93ac405385b9f8994281db7fbdeeb15
Harnoor Singh <iharnoor@gmail.com>
2024-02-24 08:41:34 -0500

    Initial Commit

---
+++ passingdata/passingdataApp.swift
@@ -0,0 +1,17 @@
+//
+//  passingdataApp.swift
+//  passingdata
+//
+//  Created by Harnoor Singh on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct passingdataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
